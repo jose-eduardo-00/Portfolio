1929fd60fc4c2939ae1d6a47928b1aea0d2e7e43
jose-eduardo-00 <dudu.gois.123@gmail.com>
2023-11-13 18:23:17 -0300

    atualizando a seção sobre

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,4 +1,4 @@
-import imgTest from '../../assets/img-test.png'
+import fotoDoDonoDoSite from '../../assets/foto_jose.jpg'
 import './about.scss'
 
 const About = () => {
@@ -6,12 +6,12 @@ const About = () => {
         <section className="about" id='about'>
             <p>
                 Eu sou o José Eduardo desenvolvedor front-end júnior, e estou em busca da minha primeira oportunidade como
-                desenvolvedor. Estou cursando Ciência da Computação e sou apaixonado por tecnologia, como desenvolvedor estou
-                sempre buscando mais conhecimento com diferentes desafios, me aprimorando e sempre tentando trazer as 
-                interfaces mais criativas e organizadas e as melhores soluções para impactar positivamente os usuários.
+                desenvolvedor.<br /> Estou cursando Ciência da Computação e sou apaixonado por tecnologia, como desenvolvedor 
+                estou sempre buscando mais conhecimento com diferentes desafios, me aprimorando e sempre tentando trazer as 
+                interfaces mais criativas e organizadas e as melhores soluções para impactar positivamente os usuários.<br />
                 Nas horas vagas eu gosto de jogar, ver filmes ou séries, assistir/ouvir podcast sobre tecnologia e cozinhar.  
             </p>
-            <img src={imgTest} alt="Foto do autor do site"></img>
+            <img src={fotoDoDonoDoSite} alt="Foto do autor do site"></img>
         </section>
     )
 }
