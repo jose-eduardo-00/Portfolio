828d1bd26b7f78a1ce73a0a611a9f45b0fd437f0
José Eduardo Gois de Pinho Costa <eduardo@softwareprecisao.com>
2025-09-23 19:05:43 -0300

    feat: alteração no fundo do site, e adição do botao de curriculo no header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,30 +1,49 @@
-import './header.scss'
-import { hoverUnderline, menuHamburguer } from './utils'
-import { useEffect } from 'react'
+import { ButtonCV } from "../buttonCV/buttonCV";
+import "./header.scss";
+import { hoverUnderline, menuHamburguer } from "./utils";
+import { useEffect } from "react";
 
 const Header = () => {
+  useEffect(() => {
+    hoverUnderline();
+  }, []);
 
-    useEffect(() => {
-        hoverUnderline()
-    }, [])
+  return (
+    <header className="header">
+      <div className="menu-hamburguer" onClick={menuHamburguer}>
+        <div className="hamburguer before"></div>
+        <div className="hamburguer middle"></div>
+        <div className="hamburguer after"></div>
+      </div>
+      <nav className="navigation">
+        <ul>
+          <li>
+            <a href="#about" className="hover-underline">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#works" className="hover-underline">
+              Works
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="hover-underline">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#contacts" className="hover-underline">
+              Contacts
+            </a>
+          </li>
+          <li>
+            <ButtonCV />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-    return (
-        <header className="header">
-            <div className='menu-hamburguer' onClick={menuHamburguer}>
-                <div className="hamburguer before"></div>
-                <div className="hamburguer middle"></div>
-                <div className="hamburguer after"></div>
-            </div>
-            <nav className='navigation'>
-                <ul>
-                    <li><a href='#about' className='hover-underline'>About</a></li>
-                    <li><a href='#works' className='hover-underline'>Works</a></li>
-                    <li><a href='#skills' className='hover-underline'>Skills</a></li>
-                    <li><a href='#contacts' className='hover-underline'>Contacts</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
-
-export { Header }
+export { Header };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,19 @@
-import { HomeScreen } from "../components/homeScreen/homeScreen"
-import { About } from "../components/about/about"
-import { Works } from "../components/works/works"
-import { Skills } from "../components/skills/skills"
-import { Contacts } from "../components/contacts/contacts"
-import { ButtonCV } from "../components/buttonCV/buttonCV"
+import { HomeScreen } from "../components/homeScreen/homeScreen";
+import { About } from "../components/about/about";
+import { Works } from "../components/works/works";
+import { Skills } from "../components/skills/skills";
+import { Contacts } from "../components/contacts/contacts";
 
 const Home = () => {
-    return (
-        <>
-            <HomeScreen />
-            <About />
-            <Works />
-            <Skills />
-            <Contacts />
-            <ButtonCV />
-        </>
-    )
-}
+  return (
+    <>
+      <HomeScreen />
+      <About />
+      <Works />
+      <Skills />
+      <Contacts />
+    </>
+  );
+};
 
-export { Home }
+export { Home };
