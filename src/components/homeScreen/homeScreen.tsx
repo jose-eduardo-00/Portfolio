0d40516c3fac15f50b5de144cef8c5cbb7ea1a55
José Eduardo Gois de Pinho Costa <eduardo@softwareprecisao.com>
2025-09-24 15:22:12 -0300

    feat: layout alterarado da home

--- src/components/homeScreen/homeScreen.tsx
+++ src/components/homeScreen/homeScreen.tsx
@@ -1,19 +1,35 @@
-import { Header } from '../header/header'
-import './homeScreen.scss'
+import { Header } from "../header/header";
+import fotoDoDonoDoSite from "../../assets/foto_jose.jpg";
+import "./homeScreen.scss";
 
 const HomeScreen = () => {
-    return (
-        <section className='home-screen'>
-            <Header />
-            <main>
-                <div className='home-screen-presentation'>
-                    <h2>Welcome, <span>I am</span></h2>
-                    <h1 className='name'>José Eduardo</h1>
-                    <h2>Front-End Developer</h2>
-                </div>
-            </main>
-        </section>
-    )
-}
+  return (
+    <section className="home-screen">
+      <Header />
+      <main>
+        <div className="home-screen-presentation">
+          <div className="text-area">
+            <h2>
+              Welcome, <span>I am</span>
+            </h2>
+            <h1 className="name">José Eduardo</h1>
+            <h3>Full-Stack Developer</h3>
+            <p>
+              Desenvolvedor Full Stack com 1 ano de experiência na criação de
+              soluções web completas e robustas. Como estudante de Ciência da
+              Computação, combino teoria e prática para atuar do front-end ao
+              back-end, transformando desafios complexos em aplicações
+              eficientes e escaláveis. Busco novas oportunidades para aplicar
+              meu conhecimento no desenvolvimento de projetos inovadores.
+            </p>
+          </div>
+          <div className="box-img">
+            <img src={fotoDoDonoDoSite} alt="Foto do autor do site"></img>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+};
 
-export { HomeScreen }
+export { HomeScreen };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { HomeScreen } from "../components/homeScreen/homeScreen";
-import { About } from "../components/about/about";
+// import { About } from "../components/about/about";
 import { Works } from "../components/works/works";
 import { Skills } from "../components/skills/skills";
 import { Contacts } from "../components/contacts/contacts";
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <>
       <HomeScreen />
-      <About />
+      {/* <About /> */}
       <Works />
       <Skills />
       <Contacts />
